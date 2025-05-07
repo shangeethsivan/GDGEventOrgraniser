2a505cbadb370ae4a38c53a5886014c0b19580e1
Shangeeth Sivan <9254310+shangeethsivan@users.noreply.github.com>
2025-05-08 00:37:04 +0530

    Update renovate-entrypoint.sh

--- .github/renovate-entrypoint.sh
+++ .github/renovate-entrypoint.sh
@@ -4,4 +4,8 @@ apt update
 
 apt install -y build-essential libpq-dev
 
-runuser -u ubuntu renovate
+docker run --rm \
+  -v $PWD:/usr/src \
+  -e LOG_LEVEL=info \
+  -e GITHUB_COM_TOKEN="${GITHUB_TOKEN}" \
+  renovate/renovate --platform github --autodiscover
